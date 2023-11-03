f75f280c769ec77c23520be5e4231639790de77c
unknown <nabilm432a@gmail.com>
2023-11-03 15:54:00 +0600

    Added display for notice board

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,16 +9,30 @@
         @section('main')
 
                 <div class="text-white" style="background-color: #2d3748">
-                    <div style="margin-left: 9rem"><h1>This is the Dashboard</h1></div>
+                    <div style="margin-left: 9rem"><h1>Dashboard</h1></div>
                 </div>
+
                 @if(!auth()->user()->passport && !auth()->user()->contact)
-                    <p style="color: yellow">Welcome, you are signed up through google, so you need to enter your passport no. and contact in your profile screen</p>
+                <div style="margin-left: 5px; margin-top: 5px; border-radius: 8px; width: 40%; height: 5%; background-color: rgb(17, 24, 39);">
+                    <p style="color: yellow; margin-left: 5px; margin-top: 5px">Welcome, you are signed up through google, so you need to enter your passport no. and contact in your profile screen</p>
+                </div>
                 @elseif(!auth()->user()->passport)
-                    <p style="color: yellow">Your Passport has not been added to your account yet</p>
+                <div style="margin-left: 5px; margin-top: 5px; border-radius: 8px; width: 40%; height: 5%; background-color: rgb(17, 24, 39);">
+                    <p style="color: yellow; margin-left: 5px; margin-top: 5px">Your Passport has not been added to your account yet</p>
+                </div>
                 @elseif(!auth()->user()->contact)
-                    <p style="color: yellow">Your Contact has not been added to your account yet</p>
+                <div style="margin-left: 5px; margin-top: 5px; border-radius: 8px; width: 40%; height: 5%; background-color: rgb(17, 24, 39);">
+                    <p style="color: yellow; margin-left: 5px; margin-top: 5px">Your Contact has not been added to your account yet</p>
+                </div>
                 @endif
+
+        <div class="text-white" style="margin-left: 5px; margin-top: 5px; border-radius: 8px; width: 40%; height: 50%; padding: 1rem;background-color: rgb(17, 24, 39);">
+            <div style="padding: 0.5rem; background-color: rgb(50, 29, 39); border-radius: 8px">
+                <p>Notice Board</p>
+            </div>
+        </div>
         @endsection
+
 
         @section('footer')
 
