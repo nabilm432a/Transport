6bffbad0a214ff5f59e6a877f13c5a39c5b4f4e3
unknown <nabilm432a@gmail.com>
2023-11-17 15:16:13 +0600

    Added Travel History migration+Controller

---
+++ app/Http/Controllers/TravelhistoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Travelhistory;
+use Illuminate\Http\Request;
+
+class TravelhistoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Travelhistory $travelhistory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Travelhistory $travelhistory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Travelhistory $travelhistory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Travelhistory $travelhistory)
+    {
+        //
+    }
+}
